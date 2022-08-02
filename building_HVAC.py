58bcf0ff4913d767ff87f8033f73fc8b7b180253
chz056 <95935283+chz056@users.noreply.github.com>
2022-08-02 16:06:37 -0700

    Update building_HVAC.py

--- building_HVAC.py
+++ building_HVAC.py
@@ -31,19 +31,19 @@ class BuildingEnvReal(gym.Env):
         self.roomnum = Parameter['roomnum']
         self.weightCmap = Parameter['weightcmap']
         self.target = Parameter['target']
-        self.gamma=Parameter['gamma'] #Discount factor for reward, change this one later
+        self.gamma=Parameter['gamma']
         self.ghi=Parameter['ghi']
         self.GroundTemp=Parameter['GroundTemp']
         self.Occupancy=Parameter['Occupancy']
         self.acmap=Parameter['ACmap']
         self.maxpower=Parameter['max_power']
         self.Occupower=0
- 
+
 
         self.timestep = Parameter['time_resolution']
-  
+
         self.Qlow = np.ones(self.roomnum, dtype=np.float32) * (-1.0)*self.acmap.astype(np.float32)+1e-12
-        # print(self.Qlow)
+
 
         self.Qhigh = np.ones(self.roomnum, dtype=np.float32) * (1.0)*self.acmap.astype(np.float32)
         self.action_space = gym.spaces.Box(self.Qlow, self.Qhigh, dtype=np.float32)
@@ -63,7 +63,7 @@ class BuildingEnvReal(gym.Env):
         np.fill_diagonal(Amatrix, np.diag(diagvalue))
         Bmatrix = self.weightCmap.T
 
- 
+
         Bmatrix[2] = self.connectmap[:, -1] * (self.RCtable[:, -1])
 
         Bmatrix = Bmatrix.T
@@ -77,13 +77,11 @@ class BuildingEnvReal(gym.Env):
         self.A_d = expm(Amatrix * self.timestep)
         self.B_d = inv(Amatrix) @ (self.A_d - np.eye(self.A_d.shape[0])) @ Bmatrix
 
-        print(np.shape(self.A_d))
-        print(np.shape(self.B_d))
 
     def step(self, action):
-        # print(self.epochs)
 
-        done = False 
+
+        done = False
         X = self.state[:self.roomnum].T
         Y = np.insert(np.append(action,self.ghi[self.epochs]), 0, self.OutTemp[self.epochs]).T
         Y = np.insert(Y, 0, self.GroundTemp[self.epochs]).T
@@ -96,34 +94,25 @@ class BuildingEnvReal(gym.Env):
         X_new = self.A_d @ X + self.B_d @ Y
 
         reward = 0
- 
-        error = X_new*self.acmap - self.target*self.acmap
-        # if np.sum(abs(error))/len(error)<=0.5:
-        #   error = error*0
-        # self.error=error
-        
-        # if np.any( abs((self.error).reshape(1,6)) >=2 ):
 
-          
-        #   reward-=100
-          
+        error = X_new*self.acmap - self.target*self.acmap
+
 
         reward -= LA.norm(action, 2) * self.q_rate + LA.norm(error, 2) * self.error_rate
         self.rewardsum += reward
 
         info = {}
-        # np.array([Meta/1000])
+
         self.state = np.concatenate((X_new, self.OutTemp[self.epochs].reshape(-1,),self.ghi[self.epochs].reshape(-1),self.GroundTemp[self.epochs].reshape(-1),np.array([self.Occupower/1000])), axis=0)
         self.statelist.append(self.state)
         self.actionlist.append(action*self.maxpower)
 
         self.epochs += 1
-        # if self.counter == 10000:
-        #   done = True
-   
+
+
         if self.epochs>=self.length_of_weather-1:
             done=True
-            #self.epochs=0
+
 
         return self.state, reward, done, info
 
@@ -138,11 +127,11 @@ class BuildingEnvReal(gym.Env):
         Meta = self.Occupancy[self.epochs]
         self.Occupower=6.461927+.946892*Meta+.0000255737*Meta**2+7.139322*avg_temp-.0627909*avg_temp*Meta+.0000589172*avg_temp*Meta**2-.19855*avg_temp**2+.000940018*avg_temp**2*Meta-.00000149532*avg_temp**2*Meta**2
         self.state=np.concatenate((self.state[:self.roomnum], self.OutTemp[self.epochs].reshape(-1,),self.ghi[self.epochs].reshape(-1),self.GroundTemp[self.epochs].reshape(-1),np.array([self.Occupower/1000])), axis=0)
-  
+
         self.flag=1
         self.rewardsum = 0
         print("Reset", self.state)
-        
+
         return self.state
 
     def render(self, mode='human'):
