c71018ee9f6a25ff757ac66959b3f4ddf9b1c089
Chi Zhang <chz056@ucsd.edu>
2024-01-17 23:36:58 -0800

    update env

--- BEAR/Env/env_building.py
+++ BEAR/Env/env_building.py
@@ -1,26 +1,16 @@
 """
-The module implements the BuildingEnvReal class.
+The module implements the BuildingEnv class.
 """
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from __future__ import annotations
+
+from typing import Any
 import functools
+import gymnasium as gym
+from gymnasium.envs.registration import EnvSpec
 import numpy as np
 from numpy import linalg as LA
-import random
-import torch.optim as optim
-import matplotlib.pyplot as plt
-import gymnasium as gym
-from scipy.linalg import expm, sinm, cosm
-from numpy.linalg import inv
-
-
-from typing import Optional
-# from gym import spaces
-# from gym.utils import seeding
+from scipy.linalg import expm
 from sklearn import linear_model
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda" if use_cuda else "cpu")
 
 
 class BuildingEnvReal(gym.Env):
@@ -52,26 +42,28 @@ class BuildingEnvReal(gym.Env):
     """
     metadata = {'render.modes': []}
 
-    #Occupancy nonlinear coefficients
-    OCCU_COEF1=6.461927
-    OCCU_COEF2=0.946892
-    OCCU_COEF3=0.0000255737
-    OCCU_COEF4=0.0627909
-    OCCU_COEF5=0.0000589172
-    OCCU_COEF6=0.19855
-    OCCU_COEF7=0.000940018
-    OCCU_COEF8=0.00000149532
+    # Occupancy nonlinear coefficients, collected from page 1299 of
+    # https://energyplus.net/assets/nrel_custom/pdfs/pdfs_v23.1.0/EngineeringReference.pdf
+    OCCU_COEF = [
+        6.461927,
+        0.946892,
+        0.0000255737,
+        0.0627909,
+        0.0000589172,
+        0.19855,
+        0.000940018,
+        0.00000149532,
+    ]
 
-    #Occupancy linear coefficient
-    OCCU_COEF9=7.139322
-
+    # Occupancy linear coefficient
+    OCCU_COEF_LINEAR = 7.139322
     #Discrete space length
     DISCRETE_LENGTH=100
 
     #Scaling factor for the reward function weight
     SCALING_FACTOR=24
 
-    def __init__(self, Parameter,user_reward_function=None,reward_breakdown_keys=None):
+    def __init__(self, Parameter: Dict[str, Any],user_reward_function=None,reward_breakdown_keys=None):
         """Initializes the environment with the given parameters.
         Args:
             Parameter (dict): Dictionary containing the parameters for the environment.
@@ -132,10 +124,20 @@ class BuildingEnvReal(gym.Env):
           self.action_space = gym.spaces.MultiDiscrete((self.Qhigh*self.DISCRETE_LENGTH-self.Qlow*self.DISCRETE_LENGTH))
 
         # Set the observation space bounds based on the minimum and maximum temperature
-        self.min_T = self.temp_range[0]
-        self.max_T = self.temp_range[1]
-        self.low = np.ones(self.roomnum*3+2, dtype=np.float32) * self.min_T
-        self.high = np.ones(self.roomnum*3+2, dtype=np.float32) * self.max_T
+        min_T, max_T = self.temp_range
+        heat_max = 1000
+        self.low = np.concatenate([
+            np.ones(self.roomnum + 1) * min_T,  # temp of zones and outdoor
+            [0],                          # GHI
+            [min_T],                      # temp of ground
+            [-min_T * self.OCCU_COEF_LINEAR / 1000]  # occupancy power
+        ]).astype(np.float32)
+        self.high = np.concatenate([
+            np.ones(self.roomnum + 1) * max_T,  # temp of zones and outdoor
+            [heat_max],                     # GHI
+            [max_T],                      # temp of ground
+            [heat_max]                      # occupancy power
+        ]).astype(np.float32)
         self.observation_space = gym.spaces.Box(self.low, self.high, dtype=np.float32)
 
         # Set the weight for the power consumption and comfort range
@@ -161,11 +163,10 @@ class BuildingEnvReal(gym.Env):
         # self._reward_breakdown = {'comfort_level':0.0,'power_consumption':0.0}
 
         # Define matrices for the state update equations
-        connect_indoor = self.connectmap[:, :-1]
         Amatrix = self.RCtable[:, :-1]
         diagvalue = (-self.RCtable) @ self.connectmap.T - np.array([self.weightCmap.T[1]]).T
         np.fill_diagonal(Amatrix, np.diag(diagvalue))
-        Amatrix+=self.nonlinear*self.OCCU_COEF9/self.roomnum
+        Amatrix+=self.nonlinear*self.OCCU_COEF_LINEAR/self.roomnum
         Bmatrix = self.weightCmap.T
         Bmatrix[2] = self.connectmap[:, -1] * (self.RCtable[:, -1])
         Bmatrix = (Bmatrix.T)
@@ -181,7 +182,7 @@ class BuildingEnvReal(gym.Env):
 
         # Compute the discrete-time system matrices
         self.A_d = expm(Amatrix * self.timestep)
-        self.B_d = inv(Amatrix) @ (self.A_d - np.eye(self.A_d.shape[0])) @ Bmatrix
+        self.B_d = LA.inv(Amatrix) @ (self.A_d - np.eye(self.A_d.shape[0])) @ Bmatrix
 
     def default_reward_function(self, state, action, error, state_new):
         # This is your default reward function
@@ -191,7 +192,7 @@ class BuildingEnvReal(gym.Env):
         self._reward_breakdown['power_consumption'] -= LA.norm(action, 2) * self.q_rate
         return reward
 
-    def step(self, action):
+    def step(self, action: np.ndarray) -> Tuple[np.ndarray, float, bool, bool, Dict[str, Any]]:
         """Steps the environment.
         Updates the state of the environment based on the given action and calculates the
         reward, done, and info values for the current timestep.
@@ -240,7 +241,8 @@ class BuildingEnvReal(gym.Env):
           Y = np.insert(Y, 0, avg_temp**2).T
         else:
           # Calculate Occupower based on the given formula
-          self.Occupower=self.OCCU_COEF1+self.OCCU_COEF2*Meta+self.OCCU_COEF3*Meta**2 - self.OCCU_COEF4*avg_temp*Meta+self.OCCU_COEF5*avg_temp*Meta**2 - self.OCCU_COEF6*avg_temp**2+self.OCCU_COEF7*avg_temp**2*Meta - self.OCCU_COEF8*avg_temp**2*Meta**2
+
+          self.Occupower = self._calc_occupower(avg_temp, Meta)
 
           # Insert Occupower at the beginning of the Y matrix
           Y = np.insert(Y, 0, self.Occupower).T
@@ -281,19 +283,22 @@ class BuildingEnvReal(gym.Env):
         # Return the new state, reward, done flag, and info
         return self.state, reward, done,done,info
 
-    def reset(self,*,seed: Optional[int] = None,T_initial: Optional[int] =0,Auto: Optional[bool] =True):
-        super().reset(seed=seed)
+    def reset(self, *, seed: int | None = None, options: dict | None = None
+              ) -> tuple[np.ndarray, dict[str, Any]]:
+        super().reset(seed=seed, options=options)
         """Resets the environment.
         Prepares the environment for the next episode by setting the initial
         temperatures, average temperature, occupancy, and occupower. The initial state
         is constructed by concatenating these variables.
 
         Args:
-            T_initial: initial temperatures of the rooms (default is 0).
-            Auto: boolean flag to set initial temperatures to the target temperatures (default is True).
-
+            seed: seed for resetting the environment. An episode is entirely
+                reproducible no matter the generator used.
+            options: resetting options
+                'verbose': set verbosity level [0-2]
         Returns:
             state: the initial state of the environment.
+            info: information.
         """
         # Initialize the episode counter
         self.epochs = 0
@@ -302,9 +307,9 @@ class BuildingEnvReal(gym.Env):
         self.statelist = []
         self.actionlist=[]
 
-        # Set initial temperatures to target temperatures if Auto is True
-        if Auto==True:
-          T_initial = self.target
+        # Use options to get T_initial or use the default value if not provided
+        T_initial = self.target if options is None else options.get('T_initial', self.target)
+
           # T_initial =np.array([18.24489859, 18.58710076, 18.47719682, 19.11476084, 19.59438163,15.39221207])
           # T_initial = np.random.uniform(21,23, self.roomnum+4)
 
@@ -315,7 +320,7 @@ class BuildingEnvReal(gym.Env):
         Meta = self.Occupancy[self.epochs]
 
         # Calculate the occupower based on occupancy and average temperature
-        self.Occupower=self.OCCU_COEF1+self.OCCU_COEF2*Meta+self.OCCU_COEF3*Meta**2 - self.OCCU_COEF4*avg_temp*Meta+self.OCCU_COEF5*avg_temp*Meta**2 - self.OCCU_COEF6*avg_temp**2+self.OCCU_COEF7*avg_temp**2*Meta - self.OCCU_COEF8*avg_temp**2*Meta**2
+        self.Occupower = self._calc_occupower(avg_temp, Meta)
 
         # Construct the initial state by concatenating relevant variables
         self.X_new=T_initial
@@ -332,7 +337,7 @@ class BuildingEnvReal(gym.Env):
 
         # Return the initial state and an empty dictionary(for gymnasium grammar)
         return self.state, self._get_info()
-    def _get_info(self, all: bool = False):
+    def _get_info(self, all: bool = False) -> Dict[str, Any]:
         """
         Returns info. See step().
 
@@ -353,8 +358,20 @@ class BuildingEnvReal(gym.Env):
                 'zone_temperature': self.X_new,
                 'reward_breakdown': self._reward_breakdown,
             }
-        return
-    def train(self,states,actions):
+        
+    def _calc_occupower(self, avg_temp: float, Meta: float) -> float:
+        return (
+            self.OCCU_COEF[0]
+            + self.OCCU_COEF[1] * Meta
+            + self.OCCU_COEF[2] * Meta**2
+            - self.OCCU_COEF[3] * avg_temp * Meta
+            + self.OCCU_COEF[4] * avg_temp * Meta**2
+            - self.OCCU_COEF[5] * avg_temp**2
+            + self.OCCU_COEF[6] * avg_temp**2 * Meta
+            - self.OCCU_COEF[7] * avg_temp**2 * Meta**2
+        )
+            
+    def train(self, states: np.ndarray, actions: np.ndarray) -> None:
         """Trains the linear regression model using the given states and actions.
         The model is trained to predict the next state based on the current state and action.
         The trained coefficients are stored in the environment for later use.
@@ -369,27 +386,27 @@ class BuildingEnvReal(gym.Env):
 
         # Iterate through states and actions to create input and output data for the model
         for i in range(len(states)-1):
-          X=states[i]
-          Y = np.insert(np.append(actions[i]/self.maxpower,self.ghi[i]), 0, self.OutTemp[i]).T
-          Y = np.insert(Y, 0, self.GroundTemp[i]).T
-          avg_temp=np.sum(X)/self.roomnum
-          Meta = self.Occupancy[i]
+            X=states[i]
+            Y = np.insert(np.append(actions[i]/self.maxpower,self.ghi[i]), 0, self.OutTemp[i]).T
+            Y = np.insert(Y, 0, self.GroundTemp[i]).T
+            avg_temp=np.sum(X)/self.roomnum
+            Meta = self.Occupancy[i]
 
           # Calculate the occupower based on occupancy and average temperature
-          self.Occupower=self.OCCU_COEF1+self.OCCU_COEF2*Meta+self.OCCU_COEF3*Meta**2 - self.OCCU_COEF4*avg_temp*Meta+self.OCCU_COEF5*avg_temp*Meta**2 - self.OCCU_COEF6*avg_temp**2+self.OCCU_COEF7*avg_temp**2*Meta - self.OCCU_COEF8*avg_temp**2*Meta**2
+            self.Occupower = self._calc_occupower(avg_temp, Meta)
 
-          # Add relevant variables to Y
-          Y = np.insert(Y, 0, Meta).T
-          Y = np.insert(Y, 0, Meta**2).T
-          Y = np.insert(Y, 0, avg_temp).T
-          Y = np.insert(Y, 0, avg_temp**2).T
+            # Add relevant variables to Y
+            Y = np.insert(Y, 0, Meta).T
+            Y = np.insert(Y, 0, Meta**2).T
+            Y = np.insert(Y, 0, avg_temp).T
+            Y = np.insert(Y, 0, avg_temp**2).T
 
-          # Concatenate X and Y to form the input data for the model
-          stackxy=np.concatenate((X,Y), axis=0)
+            # Concatenate X and Y to form the input data for the model
+            stackxy=np.concatenate((X,Y), axis=0)
 
-          # Append the input data and next state to their respective lists
-          current_state.append(stackxy)
-          next_state.append(states[i+1])
+            # Append the input data and next state to their respective lists
+            current_state.append(stackxy)
+            next_state.append(states[i+1])
 
         # Create a linear regression model with non-negative coefficients and no intercept
         model = linear_model.LinearRegression(fit_intercept=False,positive=True)
@@ -408,8 +425,8 @@ class BuildingEnvReal(gym.Env):
         self.datadriven=True
         # return current_state,next_state
 
-    def render(self, mode='human'):
+    def render(self, mode: str = 'human') -> None:
         pass
 
-    def close(self):
+    def close(self) -> None:
         pass
